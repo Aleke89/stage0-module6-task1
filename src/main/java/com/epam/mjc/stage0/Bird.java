2de07fc751041fbafaad96e9566f27468666e14e
Aleke <alnur.rysbek@mail.ru>
2022-11-07 14:43:59 +0600

    success

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,25 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal{
 
+    public Bird() {
+        super("blue", 2, false);
+    }
+    public Bird(String color, int numberOfPaws, boolean hasFur) {
+        super(color, numberOfPaws, hasFur);
+    }
+
+    @Override
+    public String getDescription() {
+        String color = getColor();
+        int pows =getNumberOfPaws();
+        if(isHasFur()==true){
+            if(pows==1){return ("This animal is mostly "+color+"."+" It has "+pows+" paw and a fur. Moreover, it has 2 wings and can fly.");
+            }
+            else {return ("This animal is mostly "+color+"."+" It has "+pows+" paws and a fur. Moreover, it has 2 wings and can fly.");}}
+        else{
+            if(pows==1){return ("This animal is mostly "+color+"."+" It has "+pows+" paw and no fur. Moreover, it has 2 wings and can fly.");}
+            else  {return ("This animal is mostly "+color+"."+" It has "+pows+" paws and no fur. Moreover, it has 2 wings and can fly.");}
+        }
+    }
 }
